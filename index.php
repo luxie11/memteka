01a7fd73074e111bfd47d8dbf76e225e2bebefbe
DeividasL <48255031+DeividasL@users.noreply.github.com>
2019-03-26 18:07:17 +0200

    su trynimu, redagavimu ir fb sidebar

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <title>Memteka</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <script async defer src="https://connect.facebook.net/lt_LT/sdk.js#xfbml=1&version=v3.2"></script>
+	<script async defer crossorigin="anonymous" src="https://connect.facebook.net/lt_LT/sdk.js#xfbml=1&version=v3.2&appId=803435083363148&autoLogAppEvents=1"></script> <!-- REIKIA FB !!!!!!!!!!!!!!!!! -->
+	
 </head>
 <body>
     <div id="wrapper">
@@ -47,16 +48,18 @@
 						$query = "SELECT * FROM memai";
 						$result = mysqli_query($dbc, $query);
 						while($row=mysqli_fetch_assoc($result))
-						{
+						{	
 							$id = $row['id']; // iskart prisiskiriame memo ID (reikalinga komentarams)
 					?>
                         <div class="meme-post">
                                 <div class="meme-content">
-					<h3 class="meme-title">
-                                    		<?php
-							echo $row['pavadinimas'];
-						?>
-					</h3>
+									<h3 class="meme-title">
+										<?php
+											echo $row['pavadinimas'];
+										?>
+										<a href="edit.php?memeId=<?php echo $id; ?>" style="color:brown;"><i class="fas fa-pen"></i></a>
+										<a href="delete_meme.php?memeId=<?php echo $id; ?>" style="color:red;" onclick="return confirm('Ar tikrai norite šalinti memą ir visus jo komentarus? Duomenys bus ištrinti negrįžtamai!')"><i class="fas fa-trash"></i></a>
+									</h3>
                                 </div>
                                 <div class="meme-image">
                                     <img src="<?php echo $row['nuoroda'];?>" alt="Smiley face">
@@ -78,7 +81,6 @@
                                 <div class="meme-buttons">
                                     <div class="control-button">
                                         <i class="fas fa-arrow-up"></i>
-										
                                     </div>
                                     <div class="control-button center-button">
                                         <i class="fas fa-arrow-down"></i>
@@ -99,7 +101,7 @@
 						?>
                 </div>
                 <div class="sidebar-column">
-                    <div style="height: 200px; background: white;"></div>
+                    <div class="fb-group" data-href="https://www.facebook.com/groups/ayearofrunning/" data-width="280" data-show-social-context="false" data-show-metadata="false"></div> <!-- REIKIA FB ! -->
                 </div>
             </div>
 			<?php
